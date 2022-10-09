282580d29e35ba9b697ac572738002e7566bb27e
zhanghang0194 <49364009+zhanghang0194@users.noreply.github.com>
2022-10-09 21:47:50 +0800

    CTRL-UNIT-UPDATING-1009-home

--- simple_cpu/simple_cpu.srcs/sources_1/new/ctrl_unit.v
+++ simple_cpu/simple_cpu.srcs/sources_1/new/ctrl_unit.v
@@ -11,7 +11,7 @@
 // Tool Versions: 
 // Description: 
 //              大体功能：根据OP指令执行各类操作；
-//              具体1：让ALU进行加减乘除与或非（不一定一致）操作，七种操作需要3bit能够覆盖
+//              具体1：让ALU进行加/减/左移/或/与/带符号比较/不带符号比较/异或操作，八种操作需要3bit能够覆盖
 //              具体2：多个MUX选择器的控制端控制（PCsrc、rt/rd、result/d_mem_out、ALUsrca、ALUsrcb）
 //              具体3：数据寄存器读写信号wr,rd；指令寄存器读写信号rw、regfile的读写信号Regwre
 //              具体4：S/Zextend模块选择信号Extsel
@@ -44,6 +44,37 @@ output regf_wre,
 output Extsel,          //待注释
 output PC_wre
     );
+
+parameter ADD = 6'b000000;//加
+parameter SUB = 6'b000001;//减
+parameter ADDIU = 6'b000010;//加
+parameter ANDI = 6'b010000;//与
+parameter AND = 6'b010001;//与
+parameter ORI = 6'b010010;//或
+parameter OR = 6'b010011;//或
+parameter SLL = 6'b011000;//移位
+
+parameter SLTI = 6'b011100;//比较
+parameter SW = 6'b100110;//寄存器读写
+parameter LW = 6'b100111;//寄存器读写
+parameter BEQ = 6'b110000;//分支1
+parameter BNE = 6'b110001;//分支2
+parameter BLTZ = 6'b110010;//分支3
+
+parameter J = 6'b111000; //跳转
+
+parameter HALT = 6'b111111;//停机
+
+parameter _ADD = 3'b000;
+parameter _SUB = 3'b001;
+parameter _SLL = 3'b010;
+parameter _OR = 3'b011;
+parameter _AND =3'b100;
+parameter _SLTU = 3'b101;
+parameter _SLT = 3'b110;
+parameter _XOR = 3'b111;
+
+assign PC_wre = op!=HALT;   //op不等于halt，PC正常工作
 
 
 endmodule
